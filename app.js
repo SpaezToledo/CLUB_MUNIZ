f128ee2cca0337516ffcaf342c715c5a1a8a57ca
SpaezToledo <santipaeztoledo@hotmail.com>
2023-04-12 10:12:22 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 2000;
+
+app.listen(port,()=> console.log('Abriendo el servidor en 2000 port'));  
+
+
+const public = path.resolve(__dirname, './public');
+        
+
+
+
+app.use(express.static(public));
+
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')))  
+
+
+
+
+
+
+
