6bd0eac1c8ec3fce825ee2635271907c809390bf
SpaezToledo <santipaeztoledo@hotmail.com>
2023-04-19 17:15:21 -0300

    Completed views, error in footer

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,34 @@
-//  window.onload = function () {
-//    alert("Bienvenido Al Club Muñiz")
-//      $("#onload").fadeOut();
-//      $("body").removeClass("hidden")
-//    }
+ window.onload = function () {
+   alert("Bienvenido Al Club Muñiz")
+     $("#onload").fadeOut();
+     $("body").removeClass("hidden")
+   }
   
+
+
+
+
+   
+
+
+
+
+   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    // Agregar un evento de clic a cada enlace
+    anchor.addEventListener('click', function (e) {
+      // Prevenir el comportamiento predeterminado del enlace
+      e.preventDefault();
   
+      // Obtener el identificador de la sección objetivo del atributo href
+      const target = document.querySelector(this.getAttribute('href'));
   
+      // Si se encuentra la sección objetivo, hacer el desplazamiento suave hacia ella
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+
+
