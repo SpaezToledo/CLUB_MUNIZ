43cdfbff4ac21b4eb7cf329440e0ae78beb47745
SpaezToledo <santipaeztoledo@hotmail.com>
2023-04-24 20:21:45 -0300

    text button

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,17 @@
- window.onload = function () {
-   alert("Bienvenido Al Club Muñiz")
-     $("#onload").fadeOut();
-     $("body").removeClass("hidden")
-   }
+//  window.onload = function () {
+//    alert("Bienvenido Al Club Muñiz")
+//      $("#onload").fadeOut();
+//      $("body").removeClass("hidden")
+//    }
   
+
+
+   // Obtener el botón y el elemento de texto
+const botonMostrar = document.getElementById('mostrar-texto');
+const textoOculto = document.getElementById('Historia');
+
+// Agregar evento de click al botón
+botonMostrar.addEventListener('click', function() {
+  // Agregar o eliminar la clase CSS "mostrar" del elemento de texto
+  textoOculto.classList.toggle('mostrar');
+});
